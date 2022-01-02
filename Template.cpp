9d0b294632bac8fa46f0c09a61b365fee9f95856
minnie-82 <86001895+minnie-82@users.noreply.github.com>
2022-01-02 19:44:50 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define nl "\n"
+
+void fast_i_o()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+
+int main()
+{
+    fast_i_o();
+    return 0;
+}
